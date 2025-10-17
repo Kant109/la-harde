1b4364dbf76423e583df71324ae5d9617fba4cca
Quentin ROULET <quentin.roulet1@gmail.com>
2025-10-17 16:04:34 +0200

    Update .env conf

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -51,7 +51,7 @@ export default defineNuxtConfig({
 
   runtimeConfig: {
     public: {
-      apiBaseUrl: process.env.API_BASE_URL || 'http://localhost:3001'
+      apiBaseUrl: import.meta.env.API_BASE_URL || 'http://localhost:3001'
     }
   },
 
